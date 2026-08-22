0be1bc91d87ac4d7cfc4be86089c7521adb5191a
azadfatehi <69792399+azadfatehi@users.noreply.github.com>
2026-08-22 12:12:49 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,8 @@
-const CACHE_NAME = 'marbles-local-v3';
-const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'icon.png',
-  'menu_bg.jpg',
-  '1.png',
-  '6.png',
-  'grass.jpg',
-  'carpet.jpg',
-  'asphalt.jpg',
-  'cement.jpg',
-  'dirt.jpg',
-  'https://jsdelivr.net'
-];
-
-// ذخیره‌سازی فوری فایل‌ها در هارد گوشی هنگام اولین اتصال
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+const CACHE_NAME = 'marble-stadium-v1';
+const ASSETS = ['index.html', 'manifest.json', '6.png', '1.png', 'emerald.png', 'ruby.png', 'cobalt.png', 'gold.png', 'grass.jpg', 'carpet.jpg', 'asphalt.jpg', 'cement.jpg', 'dirt.jpg', 'menu_bg.jpg'];
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
-
-// خواندن و اجرای سریع فایل‌ها از حافظه گوشی در حالت ۱۰۰٪ آفلاین
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
 });
