b8bb35d40b17f6c85e81652b193373c4e9cb0582
azadfatehi <69792399+azadfatehi@users.noreply.github.com>
2026-08-26 12:30:20 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,25 @@
-const CACHE_NAME = 'marble-stadium-v1';
-const ASSETS = ['index.html', 'manifest.json', '6.png', '1.png', 'emerald.png', 'ruby.png', 'cobalt.png', 'gold.png', 'grass.jpg', 'carpet.jpg', 'asphalt.jpg', 'cement.jpg', 'dirt.jpg', 'menu_bg.jpg'];
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+const CACHE_NAME = 'marbles-stadium-v1';
+const ASSETS = [
+  'index.html',
+  'shop.html', // 🏪 اضافه شدن صفحه فروشگاه برای دسترسی آفلاین
+  '6.png',
+  '1.png',
+  'grass.jpg',
+  'carpet.jpg',
+  'asphalt.jpg',
+  'cement.jpg',
+  'dirt.jpg',
+  'menu_bg.jpg'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
 });
